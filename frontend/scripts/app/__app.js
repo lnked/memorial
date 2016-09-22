4818dff4f22bca033bcbd5364d5989c3faa7e03d
ed <ed.proff@gmail.com>
2016-09-22 18:08:01 +0300

    update

--- frontend/scripts/app/__app.js
+++ frontend/scripts/app/__app.js
@@ -5,7 +5,22 @@ var app = app || {};
 
     app = {
         
+        warning: function() {
+            $('body').on('click', function(e){
+                if ($('.j-warning-block._open').length) {
+                    if (!$(e.target).closest('.j-warning-block').length && !$(e.target).hasClass('.j-warning-block')) {
+                        $('.j-warning-block._open').removeClass('_open');
+                    }
+                }
+            });
+
+            $('body').on('click', '.j-warning-trigger', function(){
+                $(this).closest('.j-warning-block').toggleClass('_open');
+            });
+        },
+
         init: function() {
+            this.warning();
             this.map.init();
             this.gallery.init();
         }
